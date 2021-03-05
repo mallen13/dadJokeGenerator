9770dbed6f3b1d710d8d952e4f127580846ad3cc
Matt Allen <bromattallen@gmail.com>
2021-03-04 19:24:13 -0500

    adding docs

---
+++ script.js
@@ -0,0 +1,46 @@
+let jokeAnswer = '';
+
+//fetch jokes
+async function fetchJokes() {
+    const setup = document.getElementById('setup');
+    
+    try {
+        const response = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/type/general');
+        const joke = await response.json();
+        setup.innerHTML = joke[0].setup;
+        jokeAnswer = joke[0].punchline;
+        
+        setTimeout( () => showText(), 1000);
+
+    } catch (error) {
+        setup.innerHTML = ('Error Retrieving Data')
+    }
+}
+
+//show 'tell me' text message
+function showText() {
+    const textMsg = document.getElementById('revealText');
+    textMsg.style.display = 'block';
+}
+
+//Show Answer button
+const revealBtn = document.getElementById('reveal');
+revealBtn.onclick = showAnswer;
+
+function showAnswer() {
+    const punchline = document.getElementById('punchline');
+    punchline.innerHTML = jokeAnswer;
+    punchline.style.display = 'block';
+    revealBtn.style.display = 'none';
+    refreshBtn.style.display = 'block';
+}
+
+//refresh button
+const refreshBtn = document.getElementById('restart');
+refreshBtn.onclick = pageRefresh;
+
+function pageRefresh() {
+    location.reload();
+}
+
+fetchJokes();
